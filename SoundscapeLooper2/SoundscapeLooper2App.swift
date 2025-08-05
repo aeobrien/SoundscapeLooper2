30760cd9fa462d346edeaec24e1fab62d7dba06b
Aidan O'Brien <aidan.e.obrien@gmail.com>
2025-08-05 17:25:27 +0100

    Initial Commit

---
+++ SoundscapeLooper2/SoundscapeLooper2App.swift
@@ -0,0 +1,17 @@
+//
+//  SoundscapeLooper2App.swift
+//  SoundscapeLooper2
+//
+//  Created by Aidan O'Brien on 05/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SoundscapeLooper2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoundscapeLooper2UITests/SoundscapeLooper2UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SoundscapeLooper2UITestsLaunchTests.swift
+//  SoundscapeLooper2UITests
+//
+//  Created by Aidan O'Brien on 05/08/2025.
+//
+
+import XCTest
+
+final class SoundscapeLooper2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
