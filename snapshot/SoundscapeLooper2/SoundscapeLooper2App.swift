2c314acc282246b3abef232f5195ed3f038e4d15
Commit Guardian <guardian@localhost>
2026-08-09 01:16:34 +0100

    Commit Guardian snapshot aidans-mac-mini 2026-08-09 SoundscapeLooper-2b4fdd6f8cd0

---
+++ snapshot/SoundscapeLooper2/SoundscapeLooper2App.swift
@@ -0,0 +1,12 @@
+
+import SwiftUI
+
+@main
+struct SoundscapeLooperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(DefaultWindowStyle())
+    }
+}
